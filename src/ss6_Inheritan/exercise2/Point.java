a7b200eb4be4c3db7799deaf7e37b4b8234a100c
nguyenhuuduc3888 <102501130+nguyenhuuduc3888@users.noreply.github.com>
2022-05-13 15:34:24 +0700

    up

--- src/ss6_Inheritan/exercise2/Point.java
+++ src/ss6_Inheritan/exercise2/Point.java
@@ -41,7 +41,7 @@ public class Point implements Comparable<Point> {
 
     @Override
     public String toString() {
-        return "(" + x + " , "+ y +")";
+        return "(" + x + " , " + y + ")";
     }
 
     @Override
--- src/ss6_Inheritan/exercise2/PointTest.java
+++ src/ss6_Inheritan/exercise2/PointTest.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class PointTest {
     public static void main(String[] args) {
-        Point point =new Point(5,6);
+        Point point = new Point(5, 6);
         System.out.println(point.toString());
         System.out.println(Arrays.toString(point.getXY()));
     }
